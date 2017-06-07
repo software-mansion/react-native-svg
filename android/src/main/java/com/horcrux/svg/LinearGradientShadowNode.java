823a05e72f88cc83ff413c6292ba4579a9fa3ac3
Mikael Sand <msand@abo.fi>
2017-06-07 16:05:41 +0300

    Implement setGradientTransform

--- android/src/main/java/com/horcrux/svg/LinearGradientShadowNode.java
+++ android/src/main/java/com/horcrux/svg/LinearGradientShadowNode.java
@@ -9,10 +9,16 @@
 
 package com.horcrux.svg;
 
+import android.graphics.Matrix;
+
+import com.facebook.common.logging.FLog;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.common.ReactConstants;
 import com.facebook.react.uimanager.annotations.ReactProp;
+
+import javax.annotation.Nullable;
 
 /**
  * Shadow node for virtual LinearGradient definition view
@@ -25,6 +31,10 @@ public class LinearGradientShadowNode extends DefinitionShadowNode {
     private String mY2;
     private ReadableArray mGradient;
     private Brush.BrushUnits mGradientUnits;
+
+    private static final float[] sMatrixData = new float[9];
+    private static final float[] sRawMatrix = new float[9];
+    protected Matrix mMatrix = new Matrix();
 
     @ReactProp(name = "x1")
     public void setX1(String x1) {
@@ -69,6 +79,31 @@ public class LinearGradientShadowNode extends DefinitionShadowNode {
         markUpdated();
     }
 
+    @ReactProp(name = "gradientTransform")
+    public void setGradientTransform(@Nullable ReadableArray matrixArray) {
+        if (matrixArray != null) {
+            int matrixSize = PropHelper.toFloatArray(matrixArray, sMatrixData);
+            if (matrixSize == 6) {
+                sRawMatrix[0] = sMatrixData[0];
+                sRawMatrix[1] = sMatrixData[2];
+                sRawMatrix[2] = sMatrixData[4] * mScale;
+                sRawMatrix[3] = sMatrixData[1];
+                sRawMatrix[4] = sMatrixData[3];
+                sRawMatrix[5] = sMatrixData[5] * mScale;
+                sRawMatrix[6] = 0;
+                sRawMatrix[7] = 0;
+                sRawMatrix[8] = 1;
+                mMatrix.setValues(sRawMatrix);
+            } else if (matrixSize != -1) {
+                FLog.w(ReactConstants.TAG, "RNSVG: Transform matrices must be of size 6");
+            }
+        } else {
+            mMatrix = null;
+        }
+
+        markUpdated();
+    }
+
     @Override
     protected void saveDefinition() {
         if (mName != null) {
@@ -80,6 +115,9 @@ public class LinearGradientShadowNode extends DefinitionShadowNode {
 
             Brush brush = new Brush(Brush.BrushType.LINEAR_GRADIENT, points, mGradientUnits);
             brush.setGradientColors(mGradient);
+            if (mMatrix != null) {
+                brush.setGradientTransform(mMatrix);
+            }
 
             SvgViewShadowNode svg = getSvgShadowNode();
             if (mGradientUnits == Brush.BrushUnits.USER_SPACE_ON_USE) {
