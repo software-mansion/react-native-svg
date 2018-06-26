d839311af3e22876650e7c942ac3dd808b0893f8
BAYU KRESNO PRASETYO <bayu.prasetyo1@btpn.com>
2018-06-26 13:35:40 +0700

    chore(Image): Update image prop type to ImageSourcePropType

--- elements/Image.js
+++ elements/Image.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Image } from "react-native";
-import { requireNativeComponent } from "react-native";
+import { Image, requireNativeComponent } from "react-native";
+import ImageSourcePropType from 'react-native/Libraries/Image/ImageSourcePropType';
 import { ImageAttributes } from "../lib/attributes";
 import { numberProp, touchableProps, responderProps } from "../lib/props";
 import Shape from "./Shape";
@@ -19,7 +19,7 @@ export default class extends Shape {
         y: numberProp,
         width: numberProp.isRequired,
         height: numberProp.isRequired,
-        href: Image.propTypes.source,
+        href: ImageSourcePropType,
         preserveAspectRatio: PropTypes.string
     };
 
