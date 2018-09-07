0fc832619e8cdf7531da0a93f85bc912e41722dc
Kenny <kt.tian@gmail.com>
2018-09-07 08:58:43 +0800

    Update TSpan.js

--- elements/TSpan.js
+++ elements/TSpan.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { requireNativeComponent } from "react-native";
-import extractText from "../lib/extract/extractTextNoCircle";
+import extractText from "../lib/extract/extractTextNoRequireCircle";
 import { textProps } from "../lib/props";
 import { TSpanAttibutes } from "../lib/attributes";
 import extractProps from "../lib/extract/extractProps";
