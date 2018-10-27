5711fd03d1af2db4f4ac42824cf1a0f7e66024a5
Jerry Thomas <geraldleethomas@hotmail.com>
2018-10-27 12:58:43 -0500

    Adds the interface for RNSVGFeGaussianBlur

---
+++ ios/Elements/RNSVGFEGaussianBlur.h
@@ -0,0 +1,10 @@
+
+#import "RNSVGNode.h"
+
+@interface RNSVGFeGaussianBlur : RNSVGNode
+
+@property (nonatomic, strong) NSString *in;
+@property (nonatomic, strong) RNSVGLength *stddeviation;
+
+
+@end
