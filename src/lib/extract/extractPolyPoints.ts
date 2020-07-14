b0efde3d4c47a488185460120babcb37d915d412
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-07-15 00:36:22 +0300

    Fix issue #1418

--- src/lib/extract/extractPolyPoints.ts
+++ src/lib/extract/extractPolyPoints.ts
@@ -3,7 +3,7 @@ import { NumberProp } from './types';
 export default function extractPolyPoints(points: string | NumberProp[]) {
   const polyPoints = Array.isArray(points) ? points.join(',') : points;
   return polyPoints
-    .replace(/[^e]-/, ' -')
+    .replace(/[^eE]-/, ' -')
     .split(/(?:\s+|\s*,\s*)/g)
     .join(' ');
 }
