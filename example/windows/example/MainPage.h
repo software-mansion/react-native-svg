a64019a2eee8e6decedd57ef928be6d6b1a2c6ca
Marlene Cota <mcota@microsoft.com>
2021-03-11 22:46:26 -0800

    add example app

---
+++ example/windows/example/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::example::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::example::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
