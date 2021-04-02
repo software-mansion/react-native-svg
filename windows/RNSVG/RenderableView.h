e685d1679c4bb8b11d1b969f00a6a384a4e3ea2f
Marlene Cota <mcota@microsoft.com>
2021-04-01 22:21:48 -0700

    RenderableView: Make properties get only

--- windows/RNSVG/RenderableView.h
+++ windows/RNSVG/RenderableView.h
@@ -18,41 +18,21 @@ struct RenderableView : RenderableViewT<RenderableView> {
   void Geometry(Microsoft::Graphics::Canvas::Geometry::CanvasGeometry value) { m_geometry = value; }
 
   hstring Id() { return m_id; }
-
   Numerics::float3x2 SvgTransform() { return m_transformMatrix; }
-  void SvgTransform(Numerics::float3x2 const &value) { m_transformMatrix = value; }
 
+  hstring FillBrushId() { return m_fillBrushId; }
   Windows::UI::Color Fill() { return m_fill; }
-  void Fill(Windows::UI::Color const &value) { m_fill = value; }
-
   float FillOpacity() { return m_fillOpacity; }
-  void FillOpacity(float value) { m_fillOpacity = value; }
-
+  hstring StrokeBrushId() { return m_strokeBrushId; }
   Windows::UI::Color Stroke() { return m_stroke; }
-  void Stroke(Windows::UI::Color const &value) { m_stroke = value; }
-
   float StrokeOpacity() { return m_strokeOpacity; }
-  void StrokeOpacity(float value) { m_strokeOpacity = value; }
-
   float StrokeMiterLimit() { return m_strokeMiterLimit; }
-  void StrokeMiterLimit(float value) { m_strokeMiterLimit = value; }
-
   float StrokeDashOffset() { return m_strokeDashOffset; }
-  void StrokeDashOffset(float value) { m_strokeDashOffset = value; }
-
   RNSVG::SVGLength StrokeWidth() { return m_strokeWidth; }
-  void StrokeWidth(RNSVG::SVGLength const &value) { m_strokeWidth = value; }
-
   Windows::Foundation::Collections::IVector<RNSVG::SVGLength> StrokeDashArray() { return m_strokeDashArray; }
-
   Microsoft::Graphics::Canvas::Geometry::CanvasCapStyle StrokeLineCap() { return m_strokeLineCap; }
-  void StrokeLineCap(Microsoft::Graphics::Canvas::Geometry::CanvasCapStyle const &value) { m_strokeLineCap = value; }
-
   Microsoft::Graphics::Canvas::Geometry::CanvasLineJoin StrokeLineJoin() { return m_strokeLineJoin; }
-  void StrokeLineJoin(Microsoft::Graphics::Canvas::Geometry::CanvasLineJoin const &value) { m_strokeLineJoin = value; }
-
   Microsoft::Graphics::Canvas::Geometry::CanvasFilledRegionDetermination FillRule() { return m_fillRule; }
-  void FillRule(Microsoft::Graphics::Canvas::Geometry::CanvasFilledRegionDetermination const &value) { m_fillRule = value; }
 
   virtual void UpdateProperties(Microsoft::ReactNative::IJSValueReader const &reader, bool forceUpdate = true, bool invalidate = true);
   virtual void CreateGeometry(Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const &/*canvas*/) {}
@@ -70,6 +50,8 @@ struct RenderableView : RenderableViewT<RenderableView> {
   Numerics::float3x2 m_transformMatrix{Numerics::make_float3x2_rotation(0)};
   Windows::UI::Color m_fill{Windows::UI::Colors::Transparent()};
   Windows::UI::Color m_stroke{Windows::UI::Colors::Transparent()};
+  hstring m_fillBrushId{L""};
+  hstring m_strokeBrushId{L""};
   float m_fillOpacity{1.0f};
   float m_strokeOpacity{1.0f};
   float m_strokeMiterLimit{0.0f};
