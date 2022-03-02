3ca5f40162062b8d7cad3bb45a28a5c29ac52599
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2022-03-02 15:45:40 +0100

    fix: fix ClipPath props

--- src/elements/ClipPath.tsx
+++ src/elements/ClipPath.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { extract } from '../lib/extract/extractProps';
 import Shape from './Shape';
 import { RNSVGClipPath } from './NativeComponents';
-import { ClipProps } from './types';
+import { ClipPathProps } from './types';
 
-export default class ClipPath extends Shape<ClipProps> {
+export default class ClipPath extends Shape<ClipPathProps> {
   static displayName = 'ClipPath';
 
   render() {
