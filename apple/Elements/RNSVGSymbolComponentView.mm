ab45eac6ca2683f0c030bcdb361fdc9fa2c5c0c1
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2022-05-12 15:34:31 +0200

    feat: add symbol and change its viewManager inheritance to match view

---
+++ apple/Elements/RNSVGSymbolComponentView.mm
@@ -0,0 +1,90 @@
+#import "RNSVGSymbolComponentView.h"
+#import "RNSVGSymbol.h"
+#import "RNSVGFabricConversions.h"
+
+#import <react/renderer/components/rnsvg/ComponentDescriptors.h>
+
+#import "RCTFabricComponentsPlugins.h"
+#import "RCTConversions.h"
+
+using namespace facebook::react;
+
+@implementation RNSVGSymbolComponentView {
+    RNSVGSymbol *_element;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+  if (self = [super initWithFrame:frame]) {
+    static const auto defaultProps = std::make_shared<const RNSVGSymbolProps>();
+    _props = defaultProps;
+    _element = [[RNSVGSymbol alloc] init];
+    self.contentView = _element;
+  }
+  return self;
+}
+
+- (void)mountChildComponentView:(UIView<RCTComponentViewProtocol> *)childComponentView index:(NSInteger)index
+{
+    if ([childComponentView isKindOfClass:[RCTViewComponentView class]] && [((RCTViewComponentView *)childComponentView).contentView isKindOfClass:[RNSVGNode class]]) {
+        [_element insertSubview:((RCTViewComponentView *)childComponentView).contentView atIndex:index];
+        [_element invalidate];
+    } else {
+        [super mountChildComponentView:childComponentView index:index];
+    }
+}
+
+- (void)unmountChildComponentView:(UIView<RCTComponentViewProtocol> *)childComponentView index:(NSInteger)index
+{
+    if ([childComponentView isKindOfClass:[RCTViewComponentView class]] && [((RCTViewComponentView *)childComponentView).contentView isKindOfClass:[RNSVGNode class]]) {
+        [childComponentView removeFromSuperview];
+        [_element invalidate];
+    } else {
+        [super unmountChildComponentView:childComponentView index:index];
+    }
+}
+
+#pragma mark - RCTComponentViewProtocol
+
++ (ComponentDescriptorProvider)componentDescriptorProvider
+{
+  return concreteComponentDescriptorProvider<RNSVGSymbolComponentDescriptor>();
+}
+
+- (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
+{
+    const auto &newProps = *std::static_pointer_cast<const RNSVGSymbolProps>(props);
+    setCommonRenderableProps(newProps, _element);
+    
+    if (RCTNSStringFromStringNilIfEmpty(newProps.fontSize)) {
+        _element.font = @{ @"fontSize": RCTNSStringFromString(newProps.fontSize) };
+    }
+    if (RCTNSStringFromStringNilIfEmpty(newProps.fontWeight)) {
+        _element.font = @{ @"fontWeight": RCTNSStringFromString(newProps.fontWeight) };
+    }
+    NSDictionary *fontDict = parseFontStruct(newProps.font);
+    if (fontDict.count > 0) {
+        _element.font = fontDict;
+    }
+    _element.minX = newProps.minX;
+    _element.minY = newProps.minY;
+    _element.vbWidth = newProps.vbWidth;
+    _element.vbHeight = newProps.vbHeight;
+    _element.align = RCTNSStringFromStringNilIfEmpty(newProps.align);
+    _element.meetOrSlice = intToRNSVGVBMOS(newProps.meetOrSlice);
+    [super updateProps:props oldProps:oldProps];
+}
+
+- (void)prepareForRecycle
+{
+    [super prepareForRecycle];
+    _element = [[RNSVGSymbol alloc] init];
+    self.contentView = _element;
+}
+
+@end
+
+Class<RCTComponentViewProtocol> RNSVGSymbolCls(void)
+{
+  return RNSVGSymbolComponentView.class;
+}
