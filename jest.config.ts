f335b9914415d7d1ffe6a9e23330e313f3e04f0b
Stephen Pittman <stephen@gomerits.com>
2022-05-27 12:50:32 +1000

    fix: make root Jest tests ignore Example folders

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  testPathIgnorePatterns: ['/node_modules/', '/Example/', '/TestsExample/'],
   preset: 'react-native',
   verbose: true,
 };
