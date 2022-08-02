fe51eae0b5801f424046b268eff86e760e24050a
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2022-08-02 11:34:57 +0200

    feat: add imgae

--- apple/Elements/RNSVGImage.mm
+++ apple/Elements/RNSVGImage.mm
@@ -26,11 +26,22 @@
 #import "RNSVGViewBox.h"
 #import "RCTBridge.h"
 
+#ifdef RN_FABRIC_ENABLED
+#import <react/renderer/components/rnsvg/ComponentDescriptors.h>
+#import "RCTFabricComponentsPlugins.h"
+#import "RCTConversions.h"
+#import <react/renderer/components/view/conversions.h>
+#import "RNSVGFabricConversions.h"
+#import "RCTImagePrimitivesConversions.h"
+#import "RCTImageSource.h"
+
 // Some RN private method hacking below similar to how it is done in RNScreens:
 // https://github.com/software-mansion/react-native-screens/blob/90e548739f35b5ded2524a9d6410033fc233f586/ios/RNSScreenStackHeaderConfig.mm#L30
 @interface RCTBridge (Private)
 + (RCTBridge *)currentBridge;
 @end
+
+#endif
 
 @implementation RNSVGImage
 {
@@ -38,6 +49,82 @@
     CGSize _imageSize;
     RCTImageLoaderCancellationBlock _reloadImageCancellationBlock;
 }
+#ifdef RN_FABRIC_ENABLED
+using namespace facebook::react;
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+  if (self = [super initWithFrame:frame]) {
+    static const auto defaultProps = std::make_shared<const RNSVGImageProps>();
+    _props = defaultProps;
+  }
+  return self;
+}
+
+#pragma mark - RCTComponentViewProtocol
+
++ (ComponentDescriptorProvider)componentDescriptorProvider
+{
+  return concreteComponentDescriptorProvider<RNSVGImageComponentDescriptor>();
+}
+
+- (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
+{
+  const auto &newProps = *std::static_pointer_cast<const RNSVGImageProps>(props);
+    const auto &oldImageProps = *std::static_pointer_cast<const RNSVGImageProps>(oldProps);
+
+    self.x = [RNSVGLength lengthWithString:RCTNSStringFromString(newProps.x)];
+    self.y = [RNSVGLength lengthWithString:RCTNSStringFromString(newProps.y)];
+    if (RCTNSStringFromStringNilIfEmpty(newProps.imageheight)) {
+        self.imageheight = [RNSVGLength lengthWithString:RCTNSStringFromString(newProps.imageheight)];
+    }
+    if (RCTNSStringFromStringNilIfEmpty(newProps.imagewidth)) {
+        self.imagewidth = [RNSVGLength lengthWithString:RCTNSStringFromString(newProps.imagewidth)];
+    }
+    if (RCTNSStringFromStringNilIfEmpty(newProps.height)) {
+        self.imageheight = [RNSVGLength lengthWithString:RCTNSStringFromString(newProps.height)];
+    }
+    if (RCTNSStringFromStringNilIfEmpty(newProps.width)) {
+        self.imagewidth = [RNSVGLength lengthWithString:RCTNSStringFromString(newProps.width)];
+    }
+    
+    if (oldProps == nullptr || oldImageProps.src != newProps.src) {
+        // TODO: make it the same as in e.g. slider
+        NSURLRequest *request = NSURLRequestFromImageSource(newProps.src);
+        CGSize size = RCTCGSizeFromSize(newProps.src.size);
+        CGFloat scale = newProps.src.scale;
+        RCTImageSource *imageSource = [[RCTImageSource alloc] initWithURLRequest:request size:size scale:scale];
+        [self setImageSrc:imageSource request:request];
+    }
+    self.align = RCTNSStringFromStringNilIfEmpty(newProps.align);
+    self.meetOrSlice = intToRNSVGVBMOS(newProps.meetOrSlice);
+    
+    setCommonRenderableProps(newProps, self, self);
+
+    [super updateProps:props oldProps:oldProps];
+}
+
+- (void)prepareForRecycle
+{
+    [super prepareForRecycle];
+    _x = nil;
+    _y = nil;
+    _imageheight = nil;
+    _imagewidth = nil;
+    _src = nil;
+    _align = nil;
+    _meetOrSlice = kRNSVGVBMOSMeet;
+    
+    if (_image) {
+        CGImageRelease(_image);
+    }
+    _image = nil;
+    _imageSize = CGSizeZero;
+    _reloadImageCancellationBlock = nil;
+    
+    [self fabricDealloc];
+}
+#endif
 
 - (void)setSrc:(id)src
 {
@@ -229,3 +316,8 @@
 }
 
 @end
+
+Class<RCTComponentViewProtocol> RNSVGImageCls(void)
+{
+  return RNSVGImage.class;
+}
