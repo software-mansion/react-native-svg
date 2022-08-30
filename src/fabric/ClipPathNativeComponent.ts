761b29d23f62154f696fb595258f22cf8a53dd0d
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2022-08-30 17:38:29 +0200

    feat: add missing props in ClipPath and Defs

--- src/fabric/ClipPathNativeComponent.ts
+++ src/fabric/ClipPathNativeComponent.ts
@@ -1,5 +1,5 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import type { ViewProps } from 'react-native';
+import type { ColorValue, ViewProps } from 'react-native';
 import type {
   Float,
   Int32,
@@ -21,6 +21,55 @@ interface SvgNodeCommonProps {
   display?: string;
 }
 
-interface NativeProps extends ViewProps, SvgNodeCommonProps {}
+type ColorStruct = Readonly<{
+  type?: WithDefault<Int32, -1>;
+  value?: ColorValue;
+  brushRef?: string;
+}>;
+
+interface SvgRenderableCommonProps {
+  fill?: ColorStruct;
+  fillOpacity?: WithDefault<Float, 1.0>;
+  fillRule?: WithDefault<Int32, 1>;
+  stroke?: ColorStruct;
+  strokeOpacity?: WithDefault<Float, 1.0>;
+  strokeWidth?: WithDefault<string, '1'>;
+  strokeLinecap?: WithDefault<Int32, 0>;
+  strokeLinejoin?: WithDefault<Int32, 0>;
+  strokeDasharray?: ReadonlyArray<string>;
+  strokeDashoffset?: Float;
+  strokeMiterlimit?: Float;
+  vectorEffect?: WithDefault<Int32, 0>;
+  propList?: ReadonlyArray<string>;
+}
+
+type FontObject = Readonly<{
+  fontStyle?: string;
+  fontVariant?: string;
+  fontWeight?: string;
+  fontStretch?: string;
+  fontSize?: string;
+  fontFamily?: string;
+  textAnchor?: string;
+  textDecoration?: string;
+  letterSpacing?: string;
+  wordSpacing?: string;
+  kerning?: string;
+  fontFeatureSettings?: string;
+  fontVariantLigatures?: string;
+  fontVariationSettings?: string;
+}>;
+
+type SvgGroupCommonProps = Readonly<{
+  fontSize?: string;
+  fontWeight?: string;
+  font?: FontObject;
+}>;
+
+interface NativeProps
+  extends ViewProps,
+    SvgNodeCommonProps,
+    SvgRenderableCommonProps,
+    SvgGroupCommonProps {}
 
 export default codegenNativeComponent<NativeProps>('RNSVGClipPath');
