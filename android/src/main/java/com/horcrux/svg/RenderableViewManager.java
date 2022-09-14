software-mansion/react-native-svg

commit 57ea7164751a00d42bfecc58408b3fab1b9dc784

fix: bitmap nulled before screen transition (#1844)
This PR fixes the issues in `react-native-screens` and probably other libraries using native `Fragments` for transitions on Android (see https://github.com/software-mansion/react-native-screens/issues/773). It moves the recycling and setting `mBitMap` to `null` to `onDetachedFromWindow` when the view is removed from `react` view hierarchy. `invalidate` is not always the proper place for doing it since the native view can still be visible for the user after the `invalidate` is called (see this comment with video: https://github.com/software-mansion/react-native-screens/issues/773#issuecomment-769418173).

android/src/main/java/com/horcrux/svg/RenderableViewManager.java
@@ -530,6 +530,10 @@ class VirtualViewManager<V extends VirtualView> extends ViewGroupManager<Virtual
           @Override
           public void onChildViewRemoved(View view, View view1) {
             if (view instanceof VirtualView) {
+              SvgView svgView = ((VirtualView) view).getSvgView();
+              if (svgView != null) {
+                svgView.setRemovedFromReactViewHierarchy();
+              }
               invalidateSvgView((V) view);
             }
           }

android/src/main/java/com/horcrux/svg/SvgView.java
@@ -59,6 +59,7 @@ public class SvgView extends FabricEnabledViewGroup
   }
 
   private @Nullable Bitmap mBitmap;
+  private boolean mRemovedFromReactViewHierarchy;
 
   public SvgView(ReactContext reactContext) {
     super(reactContext);
@@ -73,6 +74,10 @@ public class SvgView extends FabricEnabledViewGroup
     SvgViewManager.setSvgView(id, this);
   }
 
+  public void setRemovedFromReactViewHierarchy() {
+    mRemovedFromReactViewHierarchy = true;
+  }
+
   @Override
   public void invalidate() {
     super.invalidate();
@@ -85,6 +90,20 @@ public class SvgView extends FabricEnabledViewGroup
       ((VirtualView) parent).getSvgView().invalidate();
       return;
     }
+    if (!mRemovedFromReactViewHierarchy) {
+      // when view is removed from the view hierarchy, we want to recycle the mBitmap when
+      // the view is detached from window, in order to preserve it for during animation, see
+      // https://github.com/react-native-svg/react-native-svg/pull/1542
+      if (mBitmap != null) {
+        mBitmap.recycle();
+      }
+      mBitmap = null;
+    }
+  }
+
+  @Override
+  protected void onDetachedFromWindow() {
+    super.onDetachedFromWindow();
     if (mBitmap != null) {
       mBitmap.recycle();
     }

commit f16a08f40f7ebc76bb5e4ade63e23de315e7088e

fix: restore bitmap recycling on children changes (#1864)
PR fixing the regression introduced in #1844. In that PR we set mRemovedFromReactViewHierarchy after one of SvgView's children has been removed, resulting in not being able to update it after one of the components in its hierarchy has been removed. In this PR the behavior has been changed, now we subscribe to startViewTransition which should be called only by react-native-screens when the screen starts its removal animation. It should still work for old issues as well as the new ones.

android/src/main/java/com/horcrux/svg/RenderableViewManager.java
@@ -530,10 +530,6 @@ class VirtualViewManager<V extends VirtualView> extends ViewGroupManager<Virtual
           @Override
           public void onChildViewRemoved(View view, View view1) {
             if (view instanceof VirtualView) {
-              SvgView svgView = ((VirtualView) view).getSvgView();
-              if (svgView != null) {
-                svgView.setRemovedFromReactViewHierarchy();
-              }
               invalidateSvgView((V) view);
             }
           }

android/src/main/java/com/horcrux/svg/SvgView.java
@@ -59,7 +59,7 @@ public class SvgView extends ReactViewGroup implements ReactCompoundView, ReactC
   }
 
   private @Nullable Bitmap mBitmap;
-  private boolean mRemovedFromReactViewHierarchy;
+  private boolean mRemovalTransitionStarted;
 
   public SvgView(ReactContext reactContext) {
     super(reactContext);
@@ -74,10 +74,6 @@ public class SvgView extends ReactViewGroup implements ReactCompoundView, ReactC
     SvgViewManager.setSvgView(id, this);
   }
 
-  public void setRemovedFromReactViewHierarchy() {
-    mRemovedFromReactViewHierarchy = true;
-  }
-
   @Override
   public void invalidate() {
     super.invalidate();
@@ -90,7 +86,7 @@ public class SvgView extends ReactViewGroup implements ReactCompoundView, ReactC
       ((VirtualView) parent).getSvgView().invalidate();
       return;
     }
-    if (!mRemovedFromReactViewHierarchy) {
+    if (!mRemovalTransitionStarted) {
       // when view is removed from the view hierarchy, we want to recycle the mBitmap when
       // the view is detached from window, in order to preserve it for during animation, see
       // https://github.com/react-native-svg/react-native-svg/pull/1542
@@ -99,6 +95,11 @@ public class SvgView extends ReactViewGroup implements ReactCompoundView, ReactC
       }
       mBitmap = null;
     }
+  }
+
+  @Override
+  public void startViewTransition(View view) {
+    mRemovalTransitionStarted = true;
   }
 
   @Override