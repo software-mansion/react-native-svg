c6dabb21c2458256bf9d2620ad20cfc9874117ec
Marlene Cota <mcota@microsoft.com>
2022-12-02 15:41:57 -0800

    autolinking

--- Example/windows/Example/AutolinkedNativeModules.g.cpp
+++ Example/windows/Example/AutolinkedNativeModules.g.cpp
@@ -3,12 +3,16 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-svg
+#include <winrt/RNSVG.h>
+
 namespace winrt::Microsoft::ReactNative
 {
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
-    UNREFERENCED_PARAMETER(packageProviders);
+    // IReactPackageProviders from react-native-svg
+    packageProviders.Append(winrt::RNSVG::ReactPackageProvider());
 }
 
 }
