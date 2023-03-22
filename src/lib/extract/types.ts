d03604797f8f59b1c6dd40689a8174e29f179acd
toter3 <toter3@gmail.com>
2023-03-22 14:01:09 +0200

    add accessibility and testId props and virtual nodes to support e2e tests tools (#2001)
    PR adding accessibility and testId props and virtual nodes to support e2e tests tools and provide general accessibility support.
    
    Co-authored-by: yonatan.altaraz <yonatan.altaraz@khealth.ai>
    Co-authored-by: galkahana <gal.kahana@hotmail.com>

--- src/lib/extract/types.ts
+++ src/lib/extract/types.ts
@@ -276,6 +276,9 @@ export type extractedProps = {
   clipPath?: string;
   clipRule?: number;
   display?: string;
+  testID?: string;
+  accessibilityLabel?: string;
+  accessible?: boolean;
   [touchableProperty: string]: unknown;
 };
 
