ccf1261cf74456be88fa9235ddde98f3ebc52159
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-03-30 16:53:04 +0200

    feat: change condition

--- src/xml.tsx
+++ src/xml.tsx
@@ -122,7 +122,7 @@ export function SvgXml(props: XmlProps) {
 
 export async function fetchText(uri: string) {
   const response = await fetch(uri);
-  if (response.ok  || response['_bodyBlob']) {
+  if (response.ok || (response.status === 0 && uri.startsWith('file://'))) {
     return await response.text();
   }
   throw new Error(`Fetching ${uri} failed with status ${response.status}`);
