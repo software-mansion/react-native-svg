c0e5e58f9a3a281cd8a8822646dd0dbae89d897a
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-05-16 12:46:24 +0200

    fix: parsing `<` inside comments (#2277)
    # Summary
    
    When comment occurred before first tag, there was a possible parsing
    error if it contains `<` character.
    Here is an example (line 2 and 3 would cause separate errors):
    
    ```xml
    <!-- sample rectangle -->
    <!-- <sample rectangle -->
    <!-- <sample> rectangle -->
    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
      <rect width="100" height="100" x="50" y="50" fill="red" />
    </svg>
    ```
    
    Fixes #2276
    
    ## Test Plan
    
    Manual tests in `TestsExample` app. (Test2276)

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -13,6 +13,7 @@ import Test2089 from './src/Test2089';
 import Test2196 from './src/Test2196';
 import Test2266 from './src/Test2266';
 import Test1986 from './src/Test1986';
+import Test2276 from './src/Test2276';
 
 export default function App() {
   return <ColorTest />;
