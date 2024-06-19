430ebeddf202d82650a7fd6ca741a7eeb0bbbb97
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-06-19 08:56:27 +0200

    fix: ios PathMeasure problem with index out of bounds exception

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -5,6 +5,7 @@ import ColorTest from './src/ColorTest';
 import PointerEventsBoxNone from './src/PointerEventsBoxNone';
 import Test1374 from './src/Test1374';
 import Test1718 from './src/Test1718';
+import Test1740 from './src/Test1740';
 import Test1813 from './src/Test1813';
 import Test1845 from './src/Test1845';
 import Test1986 from './src/Test1986';
