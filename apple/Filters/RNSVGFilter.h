f7c2f928c74587c2c68fe2e5077019dd108bbeca
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-06-26 14:10:20 +0200

    feat: introduce Filter elementa and register filters on SvgView

---
+++ apple/Filters/RNSVGFilter.h
@@ -0,0 +1,14 @@
+#import "RNSVGNode.h"
+
+@interface RNSVGFilter : RNSVGNode
+
+@property (nonatomic, strong) RNSVGLength *x;
+@property (nonatomic, strong) RNSVGLength *y;
+@property (nonatomic, strong) RNSVGLength *width;
+@property (nonatomic, strong) RNSVGLength *height;
+@property (nonatomic, assign) RNSVGUnits filterUnits;
+@property (nonatomic, assign) RNSVGUnits primitiveUnits;
+
+- (CIImage *)applyFilter:(CIImage *)img bounds:(CGRect)imgBounds backgroundImg:(CIImage *)backgroundImg;
+
+@end
