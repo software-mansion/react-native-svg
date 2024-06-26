0070897f68dea67c7f562eac7b4520a8c9b80640
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-06-26 14:49:20 +0200

    test: add FilterImage to example app

---
+++ Example/src/examples/FilterImage/LocalImage.tsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {Image, Svg} from 'react-native-svg';
+import {FilterImage} from 'react-native-svg/filter-image';
+
+const testImage = require('../../assets/image.jpg');
+
+const FilterImageLocalExample = () => {
+  return (
+    <View>
+      <FilterImage
+        filters={[{name: 'colorMatrix', type: 'saturate', values: [0.5]}]}
+        source={testImage}
+      />
+    </View>
+  );
+};
+FilterImageLocalExample.title = 'Local image with filter';
+
+const icon = (
+  <FilterImage
+    filters={[{name: 'colorMatrix', type: 'saturate', values: [0.5]}]}
+    source={testImage}
+    width={30}
+    height={30}
+  />
+);
+
+const samples = [FilterImageLocalExample];
+export {icon, samples};
