3af7f800f74605c048a6fd3ce39f229eeff79062
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-07-02 13:49:22 +0200

    feat: rename Example -> example

--- Example/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    '@babel/plugin-proposal-export-namespace-from',
-    'react-native-reanimated/plugin',
-  ],
-};
