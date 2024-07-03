c8c9a7a2d351d8b85cbc207f6294ef03546c6198
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-03 11:04:16 +0200

    fix: extract opacity use percentage values

--- src/lib/extract/extractOpacity.ts
+++ src/lib/extract/extractOpacity.ts
@@ -1,6 +1,9 @@
 import type { NumberProp } from './types';
 
 export default function extractOpacity(opacity: NumberProp | void) {
+  if (typeof opacity === 'string' && opacity.trim().endsWith('%')) {
+    return +opacity.slice(0, -1) / 100;
+  }
   const value = +opacity;
-  return isNaN(value) ? 1 : value;
+  return isNaN(value) || value > 1 ? 1 : Math.max(value, 0);
 }
