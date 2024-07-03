36726ac6ec900179ab60b9f51821e9ab31ed44b1
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-03 11:12:22 +0200

    Revert "fix: extract opacity allow percentage values"
    This reverts commit 17040b7782d5ecaeee74a26dde167b397d35595c.

--- src/lib/extract/extractOpacity.ts
+++ src/lib/extract/extractOpacity.ts
@@ -1,9 +1,6 @@
 import type { NumberProp } from './types';
 
 export default function extractOpacity(opacity: NumberProp | void) {
-  if (typeof opacity === 'string' && opacity.trim().endsWith('%')) {
-    return +opacity.slice(0, -1) / 100;
-  }
   const value = +opacity;
-  return isNaN(value) || value > 1 ? 1 : Math.max(value, 0);
+  return isNaN(value) ? 1 : value;
 }
