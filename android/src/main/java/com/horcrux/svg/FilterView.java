17b59b2226201b99e40a657715c678add00ee8d9
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-03 10:29:45 +0200

    fix: make resultsMap private

--- android/src/main/java/com/horcrux/svg/FilterView.java
+++ android/src/main/java/com/horcrux/svg/FilterView.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.view.View;
 import com.facebook.react.bridge.Dynamic;
 import com.facebook.react.bridge.ReactContext;
@@ -12,15 +11,13 @@ import java.util.HashMap;
 
 @SuppressLint("ViewConstructor")
 class FilterView extends DefinitionView {
-  HashMap<String, Bitmap> resultsMap = new HashMap<>();
+  private final HashMap<String, Bitmap> mResultsMap = new HashMap<>();
 
   SVGLength mX;
   SVGLength mY;
   SVGLength mW;
   SVGLength mH;
 
-  // TODO implement proper support for units
-  @SuppressWarnings({"FieldCanBeLocal", "unused"})
   private FilterProperties.Units mFilterUnits;
 
   @SuppressWarnings({"FieldCanBeLocal", "unused"})
@@ -71,21 +68,22 @@ class FilterView extends DefinitionView {
   }
 
   public Bitmap applyFilter(
-      Bitmap source, Bitmap background, RectF renderableBounds, Rect canvasBounds) {
-    resultsMap.clear();
-    resultsMap.put("SourceGraphic", source);
-    resultsMap.put("SourceAlpha", FilterUtils.applySourceAlphaFilter(source));
-    resultsMap.put("BackgroundImage", background);
-    resultsMap.put("BackgroundAlpha", FilterUtils.applySourceAlphaFilter(background));
+      Bitmap source, Bitmap background, Rect renderableBounds, Rect canvasBounds) {
+    mResultsMap.clear();
+    mResultsMap.put("SourceGraphic", source);
+    mResultsMap.put("SourceAlpha", FilterUtils.applySourceAlphaFilter(source));
+    mResultsMap.put("BackgroundImage", background);
+    mResultsMap.put("BackgroundAlpha", FilterUtils.applySourceAlphaFilter(background));
 
     Bitmap res = source;
 
     for (int i = 0; i < getChildCount(); i++) {
       View node = getChildAt(i);
       if (node instanceof FilterPrimitiveView currentFilter) {
-        res = currentFilter.applyFilter(resultsMap, res);
-        if (currentFilter.mResult != null) {
-          resultsMap.put(currentFilter.mResult, res);
+        res = currentFilter.applyFilter(mResultsMap, res);
+        String resultName = currentFilter.getResult();
+        if (resultName != null) {
+          mResultsMap.put(resultName, res);
         }
       }
     }
@@ -93,13 +91,13 @@ class FilterView extends DefinitionView {
     // crop Bitmap to filter coordinates
     int x, y, width, height;
     if (this.mFilterUnits == FilterProperties.Units.USER_SPACE_ON_USE) {
-      x = (int) (this.relativeOn(this.mX, canvasBounds.width()));
-      y = (int) (this.relativeOn(this.mY, canvasBounds.height()));
+      x = (int) this.relativeOn(this.mX, canvasBounds.width());
+      y = (int) this.relativeOn(this.mY, canvasBounds.height());
       width = (int) this.relativeOn(this.mW, canvasBounds.width());
       height = (int) this.relativeOn(this.mH, canvasBounds.height());
     } else { // FilterProperties.Units.OBJECT_BOUNDING_BOX
-      x = (int) (this.relativeOnFraction(this.mX, renderableBounds.width()));
-      y = (int) (this.relativeOnFraction(this.mY, renderableBounds.height()));
+      x = (int) this.relativeOnFraction(this.mX, renderableBounds.width());
+      y = (int) this.relativeOnFraction(this.mY, renderableBounds.height());
       width = (int) this.relativeOnFraction(this.mW, renderableBounds.width());
       height = (int) this.relativeOnFraction(this.mH, renderableBounds.height());
     }
