63ca90ce2e6d20d7702f707357332cf03724828c
Bartosz Stefańczyk <bartosz.stefanczyk@swmansion.com>
2024-07-04 11:12:36 +0200

    use proper arch in handshake message

--- Example/src/e2e/TestingView.tsx
+++ Example/src/e2e/TestingView.tsx
@@ -1,58 +1,64 @@
-import React, {Component, ReactNode, useEffect, useRef, useState} from "react";
+import React, {Component, ReactNode, useEffect, useRef, useState} from 'react';
 import {Svg, Circle} from 'react-native-svg';
-import {Platform} from "react-native";
+import {Platform} from 'react-native';
 
-const wsUri = 'ws://10.0.2.2:7123'
+const wsUri = 'ws://10.0.2.2:7123';
 
 const TestingView = () => {
   const [renderedContent, setRenderedContent] = useState(<></>);
   const [wsClient, setWsClient] = useState<WebSocket>(new WebSocket(wsUri));
-  const svgWrapperRef = useRef<Svg>(null)
+  const svgWrapperRef = useRef<Svg>(null);
 
   useEffect(() => {
     wsClient.onopen = () => {
-      //todo: use proper arch here
-      wsClient.send(JSON.stringify({os: Platform.OS, version: Platform.Version, arch: "paper"}))
-    }
+      wsClient.send(
+        JSON.stringify({
+          os: Platform.OS,
+          version: Platform.Version,
+          arch: isFabric() ? 'fabric' : 'paper',
+        }),
+      );
+    };
     //todo: find out why there is 1005 close when closing WS on Jest side
-    wsClient.onerror = (err) => {
-      console.log(err)
-      if (!err) { // when gracefully stopping WS, the err is null however the callback is still being called for some reason
+    wsClient.onerror = err => {
+      console.log(err);
+      if (!err) {
+        // when gracefully stopping WS, the err is null however the callback is still being called for some reason
         return;
       }
-      console.error(`Error while connecting to E2E WebSocket server at ${wsUri}: ${err.message}. Reopen this tab to retry.`)
-    }
+      console.error(
+        `Error while connecting to E2E WebSocket server at ${wsUri}: ${err.message}. Reopen this tab to retry.`,
+      );
+    };
     wsClient.onmessage = ({data: rawMessage}) => {
-      console.log(rawMessage)
-    }
-    wsClient.onclose = (event) => {
-      console.log(`E2E WebSocket connection has been closed (${event.code})`)
-    }
-  }, [wsClient])
+      console.log(rawMessage);
+    };
+    wsClient.onclose = event => {
+      console.log(`E2E WebSocket connection has been closed (${event.code})`);
+    };
+  }, [wsClient]);
 
-  return (
-    <Svg ref={svgWrapperRef}>
-      {renderedContent}
-    </Svg>
-  )
-}
-
+  return <Svg ref={svgWrapperRef}>{renderedContent}</Svg>;
+};
 
 class TestingViewWrapper extends Component {
   static title = 'E2E Testing';
 
   render() {
-    return (
-      <TestingView/>
-    );
+    return <TestingView />;
   }
 }
 
-const samples = [TestingViewWrapper]
+const samples = [TestingViewWrapper];
 const icon = (
   <Svg height="30" width="30" viewBox="0 0 20 20">
-    <Circle cx="10" cy="10" r="8" stroke="purple" strokeWidth="1" fill="pink"/>
+    <Circle cx="10" cy="10" r="8" stroke="purple" strokeWidth="1" fill="pink" />
   </Svg>
-)
+);
 
-export {samples, icon}
+function isFabric(): boolean {
+  // @ts-expect-error nativeFabricUIManager is not yet included in the RN types
+  return !!global?.nativeFabricUIManager;
+}
+
+export {samples, icon};
