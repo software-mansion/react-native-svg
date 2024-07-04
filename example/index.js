636a738e92e7755c4cb97af2bd024d0c2ab2114c
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-07-04 22:28:33 +0200

    feat: remove web configuration and related libraries from example app

--- example/index.js
+++ example/index.js
@@ -2,13 +2,8 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-
-if (typeof document !== 'undefined') {
-  const rootTag = document.getElementById('root');
-  AppRegistry.runApplication(appName, { rootTag });
-}
