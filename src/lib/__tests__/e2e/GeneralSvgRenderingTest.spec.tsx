38138526657dd500e133c06a3734695bceb8ea4a
Bartosz Stefańczyk <bartosz.stefanczyk@swmansion.com>
2024-07-08 16:02:15 +0200

    reorganize code little bit

---
+++ src/lib/__tests__/e2e/GeneralSvgRenderingTest.spec.tsx
@@ -0,0 +1,55 @@
+import { SvgFromXml } from 'react-native-svg';
+import * as fs from 'node:fs';
+import { compareImages, sendToDeviceAndReceive } from '../../../../e2e/helpers';
+import { RenderResponse } from '../../../../e2e/types';
+import path from 'path';
+
+const width = 200;
+const height = 200;
+const maxPixelDiff = width * height * 0.005;
+const targetPixelRatio = 3.0;
+
+const testCases = fs.readdirSync(path.resolve('e2e', 'cases'));
+testCases.forEach((testCase) => {
+  test(`Web browser rendered SVG should have less than 0.05% differences between device rendered SVG (${testCase})`, async () => {
+    const testCaseSvg = path.resolve('e2e', 'cases', testCase);
+    const svgXml = fs.readFileSync(testCaseSvg).toString('utf-8');
+    const response = await sendToDeviceAndReceive<RenderResponse>({
+      type: 'renderRequest',
+      data: <SvgFromXml xml={svgXml} />,
+      height,
+      width,
+    });
+
+    const referenceFilePath = path.resolve(
+      'e2e',
+      'references',
+      testCase.replace('.svg', '.png')
+    );
+    const diffFilePath = path.resolve(
+      'e2e',
+      'diffs',
+      `${testCase.replace('.svg', '')}-diff.png`
+    );
+    const referenceFileBuffer = fs.readFileSync(referenceFilePath);
+    const renderedDataBuffer = Buffer.from(response.data, 'base64');
+
+    // Compare reference file (from /e2e/references) with SVG rendered on actual device.
+    // Reference files can be generated off of /e2e/cases with `yarn generateE2eReferences`.
+    const amountOfDifferentPixels = compareImages(
+      referenceFileBuffer,
+      renderedDataBuffer,
+      {
+        width,
+        height,
+        pixelRatio: targetPixelRatio,
+        diffFilePath,
+      }
+    );
+
+    // Check if there is more than 0.5% different pixels in whole snapshot
+    expect(amountOfDifferentPixels).toBeLessThan(
+      maxPixelDiff * targetPixelRatio
+    );
+  });
+});
