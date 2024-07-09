a4dfc2fa44e7a4badb5f77bb7936f96167bcf94c
Bartosz Stefańczyk <bartosz.stefanczyk@swmansion.com>
2024-07-09 12:42:25 +0200

    remove note for self, add comment about hardcoding deviceScaleFactor

--- e2e/generateReferences.ts
+++ e2e/generateReferences.ts
@@ -4,11 +4,13 @@ const fs = require('fs');
 
 const main = async () => {
   const browser = await puppeteer.launch()
-  // https://pptr.dev/api/puppeteer.viewport#properties => moze fix na popsute skalowanie
   const page = await browser.newPage()
   await page.setViewport({
     height: 200,
     width: 200,
+    // This is hardcoded value which makes it possible to use only devices with pixel ratio = 3. You can change it
+    // and regenerate reference images if you want to use device with different pixel ratio
+    // see: https://reactnative.dev/docs/pixelratio
     deviceScaleFactor: 3
   })
   const casesPath = path.resolve('e2e', 'cases')
