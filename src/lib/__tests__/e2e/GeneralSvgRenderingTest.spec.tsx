dab53da620ded475d5d6ce0a28148f1f2ba413f4
Bartosz Stefańczyk <bartosz.stefanczyk@swmansion.com>
2024-07-10 15:07:06 +0200

    fix platform info

--- src/lib/__tests__/e2e/GeneralSvgRenderingTest.spec.tsx
+++ src/lib/__tests__/e2e/GeneralSvgRenderingTest.spec.tsx
@@ -3,7 +3,7 @@ import * as fs from 'node:fs';
 import { compareImages, sendToDeviceAndReceive } from '../../../../e2e/helpers';
 import { RenderResponse } from '../../../../e2e/types';
 import path from 'path';
-import { addAttach } from 'jest-html-reporters/helper';
+import { addAttach, addMsg } from 'jest-html-reporters/helper';
 
 const width = 200;
 const height = 200;
@@ -13,12 +13,11 @@ const targetPixelRatio = 3.0;
 const testCases = fs.readdirSync(path.resolve('e2e', 'cases'));
 testCases.forEach((testCase) => {
   test(`Web browser rendered SVG should have less than 0.05% differences between device rendered SVG (${testCase})`, async () => {
-    await addAttach({
-      attach: JSON.stringify({
+    await addMsg({
+      message: JSON.stringify({
         os: global.os,
         arch: global.arch,
       }),
-      description: 'Platform information',
     });
     const testCaseSvg = path.resolve('e2e', 'cases', testCase);
 
