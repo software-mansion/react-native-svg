75b26d00a91f9ccaa77aefd900e291e7bf644baf
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-12 11:49:25 +0200

    fix: generate filter id based on filer props

--- src/filter-image/FilterImage.tsx
+++ src/filter-image/FilterImage.tsx
@@ -1,4 +1,11 @@
-import { ImageProps, Image as RNImage, StyleSheet, View } from 'react-native';
+import React, { useMemo } from 'react';
+import {
+  ImageProps,
+  Platform,
+  Image as RNImage,
+  StyleSheet,
+  View,
+} from 'react-native';
 import { FeColorMatrix, Filter, Image, Svg } from '../index';
 import { Filters } from './types';
 import { extractResizeMode } from './extractImage';
@@ -7,7 +14,7 @@ export interface FilterImageProps extends ImageProps {
   filters: Filters;
 }
 
-const getFilters = (filters: FilterImageProps['filters']) => {
+const getFilters = (filters: Filters) => {
   return filters?.map((filter, index) => {
     const { name, ...filterProps } = filter;
     switch (name) {
@@ -19,13 +26,32 @@ const getFilters = (filters: FilterImageProps['filters']) => {
   });
 };
 
+const getFilterName = (filter: Filters) => {
+  return (
+    'filter' +
+    filter.reduce(
+      (acc, curr) =>
+        acc +
+        curr.name +
+        (Array.isArray(curr.values) ? curr.values.join(',') : curr.values) +
+        ';',
+      ''
+    )
+  );
+};
+
 export const FilterImage = (props: FilterImageProps) => {
   const { source, style, ...imageProps } = props;
+  const hasFilter = props.filters.length > 0;
+  const filterId = useMemo(() => getFilterName(props.filters), [props.filters]);
 
-  const src = RNImage.resolveAssetSource(source);
+  // FIXME: resolveAssetSource is not available on web
+  // We need to find a way to get the source asset and its dimensions
+  const src =
+    Platform.OS !== 'web' ? RNImage.resolveAssetSource(source) : undefined;
   const styles = StyleSheet.flatten(style);
-  const width = props.width || styles?.width || src.width;
-  const height = props.height || styles?.height || src.height;
+  const width = props.width || styles?.width || src?.width;
+  const height = props.height || styles?.height || src?.height;
   const preserveAspectRatio = extractResizeMode(props.resizeMode);
 
   return (
@@ -39,14 +65,16 @@ export const FilterImage = (props: FilterImageProps) => {
         },
       ]}>
       <Svg width="100%" height="100%">
-        <Filter id={`filter`}>{getFilters(props.filters)}</Filter>
+        {hasFilter && (
+          <Filter id={filterId}>{getFilters(props.filters)}</Filter>
+        )}
         <Image
           {...imageProps}
           href={props.src || props.source}
           width="100%"
           height="100%"
           preserveAspectRatio={preserveAspectRatio}
-          filter={props.filters && 'url(#filter)'}
+          filter={hasFilter ? `url(#${filterId})` : undefined}
         />
       </Svg>
     </View>
