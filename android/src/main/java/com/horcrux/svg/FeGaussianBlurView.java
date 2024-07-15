1f3fc111d9b13b3ca7c57f53f2b11e28d420e15d
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-15 11:21:20 +0200

    feat: implement FeGaussianBlur on android with limited support by RenderScript

---
+++ android/src/main/java/com/horcrux/svg/FeGaussianBlurView.java
@@ -0,0 +1,74 @@
+package com.horcrux.svg;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import com.facebook.react.bridge.ReactContext;
+import java.util.HashMap;
+
+@SuppressLint("ViewConstructor")
+class FeGaussianBlurView extends FilterPrimitiveView {
+  String mIn1;
+  float mStdDeviationX;
+  float mStdDeviationY;
+  FilterProperties.EdgeMode mEdgeMode;
+
+  public FeGaussianBlurView(ReactContext reactContext) {
+    super(reactContext);
+  }
+
+  public void setIn1(String in1) {
+    this.mIn1 = in1;
+    invalidate();
+  }
+
+  public void setStdDeviationX(float stdDeviationX) {
+    this.mStdDeviationX = stdDeviationX;
+    invalidate();
+  }
+
+  public void setStdDeviationY(float stdDeviationY) {
+    this.mStdDeviationY = stdDeviationY;
+    invalidate();
+  }
+
+  public void setEdgeMode(String edgeMode) {
+    this.mEdgeMode = FilterProperties.EdgeMode.getEnum(edgeMode);
+    invalidate();
+  }
+
+  @Override
+  public Bitmap applyFilter(HashMap<String, Bitmap> resultsMap, Bitmap prevResult) {
+    Bitmap source = getSource(resultsMap, prevResult, this.mIn1);
+    return blur(getContext(), source);
+  }
+
+  private Bitmap blur(Context context, Bitmap bitmap) {
+    float stdDeviation = Math.max(mStdDeviationX, mStdDeviationY);
+    if (stdDeviation <= 0) return bitmap;
+    final float maxRadius = 25.0f;
+    float radius = Math.min(stdDeviation, maxRadius);
+
+    Bitmap outputBitmap = Bitmap.createBitmap(bitmap);
+
+    RenderScript rs = RenderScript.create(context);
+    ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+    Allocation tmpIn = Allocation.createFromBitmap(rs, bitmap);
+    Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+
+    blurScript.setRadius(radius);
+    blurScript.setInput(tmpIn);
+    blurScript.forEach(tmpOut);
+
+    tmpOut.copyTo(outputBitmap);
+
+    rs.destroy();
+
+    return Bitmap.createScaledBitmap(outputBitmap, bitmap.getWidth(), bitmap.getHeight(), false);
+  }
+}
