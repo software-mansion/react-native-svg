80b4ddee66a541dfa359e516f9710c2b0284a721
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-19 16:56:47 +0200

    revert test

--- android/src/paper/java/com/facebook/react/viewmanagers/RNSVGLineManagerInterface.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/RNSVGLineManagerInterface.java
@@ -41,6 +41,7 @@ public interface RNSVGLineManagerInterface<T extends View> {
   void setStrokeMiterlimit(T view, float value);
   void setVectorEffect(T view, int value);
   void setPropList(T view, @Nullable ReadableArray value);
+  void setFilter(T view, @Nullable String value);
   void setX1(T view, Dynamic value);
   void setY1(T view, Dynamic value);
   void setX2(T view, Dynamic value);
