software-mansion/react-native-svg

commit 74445cc0fd1dfde70bef4abe98bcdf6272fae5de

fix: allow to array style prop (#2189)
# Summary
- Previously, styles in "Array" format were not being applied correctly.
- Therefore, now ensure that styles in "Array" format are correctly
applied using `StyleSheet.flatten.`


**Before: The `opacity` attribute is not correctly applied in
`gStyle`.** :x:
```tsx
const gStyle = Object.assign({}, [{ opacity: 0.5 }]);
/* { 0: {opacity: 0.5 }} */
```

**After: Now, the `opacity` attribute is correctly applied in
`gStyle`.** ✅
```tsx
const gStyle = StyleSheet.flatten([{ opacity: 0.5 }]);
/* { opacity: 0.5 } */
```

```tsx
const gStyle = StyleSheet.flatten([{ opacity: 0.5 }, { backgroundColor: 'red' }]);
/* { opacity: 0.5, backgroundColor: 'red' } */
```

## Test Plan

`0` key is not allow to `ViewStyle`

### What's required for testing (prerequisites)?

## Compatibility

| OS      | Implemented |
| ------- | :---------: |
| iOS     |    ✅    |
| Android |    ✅     |

## Checklist

<!-- Check completed item, when applicable, via: [X] -->

- [x] I have tested this on a device and a simulator

---------

Co-authored-by: bohdanprog <bohdan.artiukhov@swmansion.com>

src/elements/Svg.tsx
@@ -177,7 +177,7 @@ export default class Svg extends Shape<SvgProps> {
       props.onLayout = onLayout;
     }
 
-    const gStyle = Object.assign({}, style) as ViewStyle;
+    const gStyle = StyleSheet.flatten(style);
     // if transform prop is of RN style's kind, we want `SvgView` to handle it
     // since it can be done here. Otherwise, if transform is of `svg` kind, e.g. string,
     // we want G element to parse it since `Svg` does not include parsing of those custom transforms.

commit 657a102b5fc29ff47724e2cf76b0f26f1cdd2129

fix: cannot set property transform of undefined (gStyle) (#2355)
# Summary

Fix bug introduced in #2189, 
if `style` is not provided, then `StyleSheet.flatten` returns
`undefined`, and we're trying to access `transform` of it.

## Test Plan

Run `example` app

src/elements/Svg.tsx
@@ -177,7 +177,7 @@ export default class Svg extends Shape<SvgProps> {
       props.onLayout = onLayout;
     }
 
-    const gStyle = StyleSheet.flatten(style);
+    const gStyle = Object.assign({}, StyleSheet.flatten(style));
     // if transform prop is of RN style's kind, we want `SvgView` to handle it
     // since it can be done here. Otherwise, if transform is of `svg` kind, e.g. string,
     // we want G element to parse it since `Svg` does not include parsing of those custom transforms.