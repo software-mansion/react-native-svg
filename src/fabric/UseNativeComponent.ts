fd06e13bf2d3476078a61de0fe7c9bf677cdbd35
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-22 10:22:01 +0200

    Update UseNativeComponent.ts

--- src/fabric/UseNativeComponent.ts
+++ src/fabric/UseNativeComponent.ts
@@ -45,7 +45,6 @@ interface SvgRenderableCommonProps {
   strokeMiterlimit?: Float;
   vectorEffect?: WithDefault<Int32, 0>;
   propList?: ReadonlyArray<string>;
-  filter?: string;
 }
 
 interface NativeProps
