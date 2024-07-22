88a523ce2ed9112c370f9bb89ba792f602114067
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-22 10:03:27 +0200

    feat: autogenerate old code

--- android/src/paper/java/com/horcrux/svg/NativeSvgRenderableModuleSpec.java
+++ android/src/paper/java/com/horcrux/svg/NativeSvgRenderableModuleSpec.java
@@ -15,6 +15,7 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReactModuleWithSpec;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.turbomodule.core.interfaces.TurboModule;
@@ -22,7 +23,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public abstract class NativeSvgRenderableModuleSpec extends ReactContextBaseJavaModule
-    implements TurboModule {
+    implements ReactModuleWithSpec, TurboModule {
   public static final String NAME = "RNSVGRenderableModule";
 
   public NativeSvgRenderableModuleSpec(ReactApplicationContext reactContext) {
