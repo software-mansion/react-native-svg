e66d91e68d663d7c193d4752d91b23add0966d1b
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-23 15:04:07 +0200

    fix: rename StandardAttributes to CommonProps

--- src/fabric/FeOffsetNativeComponent.ts
+++ src/fabric/FeOffsetNativeComponent.ts
@@ -4,7 +4,7 @@ import type { ViewProps } from './utils';
 import { NumberProp } from '../lib/extract/types';
 import type { UnsafeMixed } from './codegenUtils';
 
-interface FilterPrimitiveStandardAttributes {
+interface FilterPrimitiveCommonProps {
   x?: UnsafeMixed<NumberProp>;
   y?: UnsafeMixed<NumberProp>;
   width?: UnsafeMixed<NumberProp>;
@@ -12,9 +12,7 @@ interface FilterPrimitiveStandardAttributes {
   result?: string;
 }
 
-export interface NativeProps
-  extends ViewProps,
-    FilterPrimitiveStandardAttributes {
+export interface NativeProps extends ViewProps, FilterPrimitiveCommonProps {
   in1?: string;
   dx?: UnsafeMixed<NumberProp>;
   dy?: UnsafeMixed<NumberProp>;
