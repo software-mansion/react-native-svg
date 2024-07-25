8710d6239828623d14f507342bea88d1e9e3daa6
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-07-25 12:40:28 +0200

    docs: add FeMerge examples

--- apps/examples/src/examples/Filters/examples.tsx
+++ apps/examples/src/examples/Filters/examples.tsx
@@ -1,5 +1,12 @@
 import * as FeColorMatrix from './FeColorMatrix';
 import * as FeGaussianBlur from './FeGaussianBlur';
+import * as FeMerge from './FeMerge';
 import * as FeOffset from './FeOffset';
 import * as ReanimatedFeColorMatrix from './ReanimatedFeColorMatrix';
-export {FeColorMatrix, FeGaussianBlur, FeOffset, ReanimatedFeColorMatrix};
+export {
+  FeColorMatrix,
+  FeGaussianBlur,
+  FeMerge,
+  FeOffset,
+  ReanimatedFeColorMatrix,
+};
