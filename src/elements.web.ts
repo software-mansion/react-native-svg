e8ed74fffa1a63ba70907cabb642949d4311f7cc
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-08-01 15:29:25 +0200

    feat: placeholders for unimplemented filters and support for Web (#2392)

--- src/elements.web.ts
+++ src/elements.web.ts
@@ -1,11 +1,33 @@
 import type { CircleProps } from './elements/Circle';
 import type { ClipPathProps } from './elements/ClipPath';
 import type { EllipseProps } from './elements/Ellipse';
+import type { FeBlendProps } from './elements/filters/FeBlend';
 import type { FeColorMatrixProps } from './elements/filters/FeColorMatrix';
+import type { FeComponentTransferProps } from './elements/filters/FeComponentTransfer';
+import type {
+  FeFuncAProps,
+  FeFuncBProps,
+  FeFuncGProps,
+  FeFuncRProps,
+} from './elements/filters/FeComponentTransferFunction';
+import type { FeCompositeProps } from './elements/filters/FeComposite';
+import type { FeConvolveMatrixProps } from './elements/filters/FeConvolveMatrix';
+import type { FeDiffuseLightingProps } from './elements/filters/FeDiffuseLighting';
+import type { FeDisplacementMapProps } from './elements/filters/FeDisplacementMap';
+import type { FeDistantLightProps } from './elements/filters/FeDistantLight';
+import type { FeDropShadowProps } from './elements/filters/FeDropShadow';
+import type { FeFloodProps } from './elements/filters/FeFlood';
 import type { FeGaussianBlurProps } from './elements/filters/FeGaussianBlur';
+import type { FeImageProps } from './elements/filters/FeImage';
 import type { FeMergeProps } from './elements/filters/FeMerge';
 import type { FeMergeNodeProps } from './elements/filters/FeMergeNode';
+import type { FeMorphologyProps } from './elements/filters/FeMorphology';
 import type { FeOffsetProps } from './elements/filters/FeOffset';
+import type { FePointLightProps } from './elements/filters/FePointLight';
+import type { FeSpecularLightingProps } from './elements/filters/FeSpecularLighting';
+import type { FeSpotLightProps } from './elements/filters/FeSpotLight';
+import type { FeTileProps } from './elements/filters/FeTile';
+import type { FeTurbulenceProps } from './elements/filters/FeTurbulence';
 import type { FilterProps } from './elements/filters/Filter';
 import type { ForeignObjectProps } from './elements/ForeignObject';
 import type { GProps } from './elements/G';
@@ -47,12 +69,76 @@ export class Ellipse extends WebShape<BaseProps & EllipseProps> {
   tag = 'ellipse' as const;
 }
 
+export class FeBlend extends WebShape<BaseProps & FeBlendProps> {
+  tag = 'feBlend' as const;
+}
+
 export class FeColorMatrix extends WebShape<BaseProps & FeColorMatrixProps> {
   tag = 'feColorMatrix' as const;
 }
 
+export class FeComponentTransfer extends WebShape<
+  BaseProps & FeComponentTransferProps
+> {
+  tag = 'feComponentTransfer' as const;
+}
+
+export class FeComposite extends WebShape<BaseProps & FeCompositeProps> {
+  tag = 'feComposite' as const;
+}
+
+export class FeConvolveMatrix extends WebShape<
+  BaseProps & FeConvolveMatrixProps
+> {
+  tag = 'feConvolveMatrix' as const;
+}
+
+export class FeDiffuseLighting extends WebShape<
+  BaseProps & FeDiffuseLightingProps
+> {
+  tag = 'feDiffuseLighting' as const;
+}
+
+export class FeDisplacementMap extends WebShape<
+  BaseProps & FeDisplacementMapProps
+> {
+  tag = 'feDisplacementMap' as const;
+}
+
+export class FeDistantLight extends WebShape<BaseProps & FeDistantLightProps> {
+  tag = 'feDistantLight' as const;
+}
+
+export class FeDropShadow extends WebShape<BaseProps & FeDropShadowProps> {
+  tag = 'feDropShadow' as const;
+}
+
+export class FeFlood extends WebShape<BaseProps & FeFloodProps> {
+  tag = 'feFlood' as const;
+}
+
+export class FeFuncA extends WebShape<BaseProps & FeFuncAProps> {
+  tag = 'feFuncA' as const;
+}
+
+export class FeFuncB extends WebShape<BaseProps & FeFuncBProps> {
+  tag = 'feFuncB' as const;
+}
+
+export class FeFuncG extends WebShape<BaseProps & FeFuncGProps> {
+  tag = 'feFuncG' as const;
+}
+
+export class FeFuncR extends WebShape<BaseProps & FeFuncRProps> {
+  tag = 'feFuncR' as const;
+}
+
 export class FeGaussianBlur extends WebShape<BaseProps & FeGaussianBlurProps> {
   tag = 'feGaussianBlur' as const;
+}
+
+export class FeImage extends WebShape<BaseProps & FeImageProps> {
+  tag = 'feImage' as const;
 }
 
 export class FeMerge extends WebShape<BaseProps & FeMergeProps> {
@@ -63,8 +149,34 @@ export class FeMergeNode extends WebShape<BaseProps & FeMergeNodeProps> {
   tag = 'feMergeNode' as const;
 }
 
+export class FeMorphology extends WebShape<BaseProps & FeMorphologyProps> {
+  tag = 'feMorphology' as const;
+}
+
 export class FeOffset extends WebShape<BaseProps & FeOffsetProps> {
   tag = 'feOffset' as const;
+}
+
+export class FePointLight extends WebShape<BaseProps & FePointLightProps> {
+  tag = 'fePointLight' as const;
+}
+
+export class FeSpecularLighting extends WebShape<
+  BaseProps & FeSpecularLightingProps
+> {
+  tag = 'feSpecularLighting' as const;
+}
+
+export class FeSpotLight extends WebShape<BaseProps & FeSpotLightProps> {
+  tag = 'feSpotLight' as const;
+}
+
+export class FeTile extends WebShape<BaseProps & FeTileProps> {
+  tag = 'feTile' as const;
+}
+
+export class FeTurbulence extends WebShape<BaseProps & FeTurbulenceProps> {
+  tag = 'feTurbulence' as const;
 }
 
 export class Filter extends WebShape<BaseProps & FilterProps> {
