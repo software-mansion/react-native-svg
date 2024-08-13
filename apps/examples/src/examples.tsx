3d6da8f740a85ff7b71650d6e5b3624fcef16c21
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-08-13 13:40:40 +0200

    feat: update tsconfig.json and update import in examples.tsx

--- apps/examples/src/examples.tsx
+++ apps/examples/src/examples.tsx
@@ -19,7 +19,7 @@ import * as Reanimated from './examples/Reanimated';
 import * as Transforms from './examples/Transforms';
 import * as Markers from './examples/Markers';
 import * as Mask from './examples/Mask';
-import * as E2E from './e2e/TestingView'
+import * as E2E from './e2e/TestingView';
 import * as Filters from './examples/Filters';
 import * as FilterImage from './examples/FilterImage';
 
