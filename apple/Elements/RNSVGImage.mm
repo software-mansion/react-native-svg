b0de2a9ad0a99ba9465d8690eaf54344a15b581a
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-08-19 10:38:25 +0200

    fix: implement protocol on RNSVGImage.mm

--- apple/Elements/RNSVGImage.mm
+++ apple/Elements/RNSVGImage.mm
@@ -151,6 +151,23 @@ using namespace facebook::react;
   });
 }
 
+- (void)didReceiveFailure:(nonnull NSError *)error fromObserver:(nonnull const void *)observer
+{
+  if (_image) {
+    CGImageRelease(_image);
+  }
+  _image = nil;
+}
+
+- (void)didReceiveProgress:(float)progress
+                    loaded:(int64_t)loaded
+                     total:(int64_t)total
+              fromObserver:(nonnull const void *)observer
+{
+}
+
+#pragma mark - RCTImageResponseDelegate - < RN 0.75
+
 - (void)didReceiveProgress:(float)progress fromObserver:(void const *)observer
 {
 }
@@ -183,6 +200,7 @@ using namespace facebook::react;
   _imageSize = CGSizeZero;
   _reloadImageCancellationBlock = nil;
 }
+
 #endif // RCT_NEW_ARCH_ENABLED
 
 - (void)setSrc:(RCTImageSource *)src
@@ -218,7 +236,7 @@ using namespace facebook::react;
 #if TARGET_OS_OSX // [macOS]
                            sourceLoaded = [src imageSourceWithSize:image.size scale:1];
 #else
-                            sourceLoaded = [src imageSourceWithSize:image.size scale:image.scale];
+                sourceLoaded = [src imageSourceWithSize:image.size scale:image.scale];
 #endif
                            NSDictionary *dict = @{
                              @"uri" : sourceLoaded.request.URL.absoluteString,
