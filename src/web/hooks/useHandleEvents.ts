877299a8249a8076a516fad4e8d81edb231e6406
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-08-20 13:33:37 +0200

    chore: add eslint-disable-next-line import/no-unresolved on useHandleEvents hook

--- src/web/hooks/useHandleEvents.ts
+++ src/web/hooks/useHandleEvents.ts
@@ -1,7 +1,9 @@
 import { MutableRefObject, useMemo } from 'react';
 // @ts-ignore not exported by react-native-web
+// eslint-disable-next-line import/no-unresolved
 import usePressEvents from 'react-native-web/dist/modules/usePressEvents';
 // @ts-ignore not exported by react-native-web
+// eslint-disable-next-line import/no-unresolved
 import useResponderEvents from 'react-native-web/dist/modules/useResponderEvents';
 import { hasResponderEvents, hasTouchableProperty } from '../utils/utils';
 import { CreateComponentProps } from '../../types';
