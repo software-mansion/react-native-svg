bacece855e06c0251bb56b934d0f9a6f3e55c3e5
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-08-20 13:25:21 +0200

    chore: remove babel.config.web and remove changes from babel.config in example app

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,15 +2,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     '@babel/plugin-proposal-export-namespace-from',
-    [
-      'module-resolver',
-      {
-        extensions: ['.js', '.ts', '.tsx'],
-        alias: {
-          react: './node_modules/react',
-        },
-      },
-    ],
     'react-native-reanimated/plugin',
   ],
 };
