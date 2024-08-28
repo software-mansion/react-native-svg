21ef4c06cdc2b61ca921fb80c90f6915c8f62443
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-08-28 15:30:31 +0200

    feat: implement ios composite filters

---
+++ apple/Filters/RNSVGFeComposite.h
@@ -0,0 +1,14 @@
+#import "RNSVGCompositeOperator.h"
+#import "RNSVGFilterPrimitive.h"
+
+@interface RNSVGFeComposite : RNSVGFilterPrimitive
+
+@property (nonatomic, strong) NSString *in1;
+@property (nonatomic, strong) NSString *in2;
+@property (nonatomic, assign) RNSVGCompositeOperator operator1;
+@property (nonatomic, strong) NSNumber *k1;
+@property (nonatomic, strong) NSNumber *k2;
+@property (nonatomic, strong) NSNumber *k3;
+@property (nonatomic, strong) NSNumber *k4;
+
+@end
--- apple/Utils/RNSVGConvert.h
+++ apple/Utils/RNSVGConvert.h
@@ -1,6 +1,7 @@
 #ifdef RCT_NEW_ARCH_ENABLED
 #import <react/renderer/components/rnsvg/Props.h>
 #import "RNSVGColorMatrixType.h"
+#import "RNSVGCompositeOperator.h"
 #import "RNSVGEdgeMode.h"
 #import "RNSVGUnits.h"
 
@@ -11,6 +12,7 @@ namespace react = facebook::react;
 + (RNSVGUnits)RNSVGUnitsFromFilterUnitsCppEquivalent:(react::RNSVGFilterFilterUnits)svgUnits;
 + (RNSVGUnits)RNSVGUnitsFromPrimitiveUnitsCppEquivalent:(react::RNSVGFilterPrimitiveUnits)svgUnits;
 + (RNSVGColorMatrixType)RNSVGColorMatrixTypeFromCppEquivalent:(react::RNSVGFeColorMatrixType)type;
++ (RNSVGCompositeOperator)RNSVGRNSVGCompositeOperatorFromCppEquivalent:(react::RNSVGFeCompositeOperator1)operator1;
 + (RNSVGEdgeMode)RNSVGEdgeModeFromCppEquivalent:(react::RNSVGFeGaussianBlurEdgeMode)edgeMode;
 
 @end
