ab17709c32ce24693a6f4e7e38a035c72c565522
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-09-24 14:27:53 +0200

    chore: declare module for useMergeRefs

--- src/web/react-native-web.d.ts
+++ src/web/react-native-web.d.ts
@@ -1,3 +1,4 @@
 'use strict';
 declare module 'react-native-web/dist/modules/usePressEvents';
 declare module 'react-native-web/dist/modules/useResponderEvents';
+declare module 'react-native-web/dist/modules/useMergeRefs';
