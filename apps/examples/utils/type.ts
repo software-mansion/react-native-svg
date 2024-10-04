b95035567a138637caebc64cf65626404ef38b7f
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-10-04 14:45:14 +0200

    feat: add Expamples type

--- apps/examples/utils/type.ts
+++ apps/examples/utils/type.ts
@@ -1,3 +1,13 @@
 import * as examples from '../src/examples';
 
 export type ExamplesKey = keyof typeof examples | 'E2E';
+
+export interface Example {
+  icon?: React.JSX.Element;
+  title: string;
+  samples: React.FC[];
+  missingOnFabric?: boolean;
+  shouldBeRenderInView?: boolean;
+}
+
+export type Examples = Record<string, Example>;
