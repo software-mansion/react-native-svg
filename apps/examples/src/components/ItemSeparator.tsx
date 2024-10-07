5b7dd89a6264f931828f461b1fd250e0c313dd07
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-10-07 15:51:57 +0200

    feat: create Item and ItemSeparator components

---
+++ apps/examples/src/components/ItemSeparator.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View} from 'react-native';
+import {commonStyles} from '../commonStyles';
+
+export function ItemSeparator() {
+  return <View style={commonStyles.separator} />;
+}
