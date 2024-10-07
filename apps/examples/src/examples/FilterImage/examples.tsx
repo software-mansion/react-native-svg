edb2ee4b7c323ae612364590c4f6fd8545119ea7
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-10-07 16:10:26 +0200

    chore: change export for FilterImageExamples

--- apps/examples/src/examples/FilterImage/examples.tsx
+++ apps/examples/src/examples/FilterImage/examples.tsx
@@ -1,23 +1,11 @@
 import * as LocalImage from './LocalImage';
 import * as RemoteImage from './RemoteImage';
 import * as FilterPicker from './FilterPicker';
-import {Example} from '../../examples';
-export {LocalImage, RemoteImage, FilterPicker};
 
-export const EXAMPLES: Record<string, Example> = {
-  LocalImageExample: {
-    icon: LocalImage.icon,
-    title: 'LocalImage',
-    samples: LocalImage.samples,
-  },
-  RemoteImageExample: {
-    icon: RemoteImage.icon,
-    title: 'RemoteImage',
-    samples: RemoteImage.samples,
-  },
-  FilterPickerExample: {
-    icon: FilterPicker.icon,
-    title: 'FilterPicker',
-    samples: FilterPicker.samples,
-  },
+import type {Examples} from '../../../utils/type';
+
+export const FilterImageExamples: Examples = {
+  LocalImage,
+  RemoteImage,
+  FilterPicker,
 };
