6d314efd7ace61f89b2de9e951c1fc284b47d768
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-10-07 16:50:37 +0200

    chore: update export for filterExamples

--- apps/examples/src/examples/Filters/examples.tsx
+++ apps/examples/src/examples/Filters/examples.tsx
@@ -1,41 +1,14 @@
-import {Example} from '../../examples';
 import * as FeColorMatrix from './FeColorMatrix';
 import * as FeGaussianBlur from './FeGaussianBlur';
 import * as FeMerge from './FeMerge';
 import * as FeOffset from './FeOffset';
 import * as ReanimatedFeColorMatrix from './ReanimatedFeColorMatrix';
-export {
+import type {Examples} from '../../../utils/type';
+
+export const FilterExamples: Examples = {
   FeColorMatrix,
   FeGaussianBlur,
   FeMerge,
   FeOffset,
   ReanimatedFeColorMatrix,
-};
-
-export const EXAMPLES: Record<string, Example> = {
-  FeColorMatrixExample: {
-    icon: FeColorMatrix.icon,
-    title: 'FeColorMatrix',
-    samples: FeColorMatrix.samples,
-  },
-  FeGaussianBlurExample: {
-    icon: FeGaussianBlur.icon,
-    title: 'FeGaussianBlur',
-    samples: FeGaussianBlur.samples,
-  },
-  FeMergeExample: {
-    icon: FeMerge.icon,
-    title: 'FeMerge',
-    samples: FeMerge.samples,
-  },
-  FeOffsetExample: {
-    icon: FeOffset.icon,
-    title: 'FeOffset',
-    samples: FeOffset.samples,
-  },
-  ReanimatedFeColorMatrixExample: {
-    icon: ReanimatedFeColorMatrix.icon,
-    title: 'ReanimatedFeColorMatrix',
-    samples: ReanimatedFeColorMatrix.samples,
-  },
 };
