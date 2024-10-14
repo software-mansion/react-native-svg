0d408ff3d329ade8bc404dd69417079bae038311
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-10-14 16:04:00 +0200

    docs: add FeFlood to examples

--- apps/examples/src/examples/Filters/examples.tsx
+++ apps/examples/src/examples/Filters/examples.tsx
@@ -1,10 +1,12 @@
 import * as FeColorMatrix from './FeColorMatrix';
+import * as FeFlood from './FeFlood';
 import * as FeGaussianBlur from './FeGaussianBlur';
 import * as FeMerge from './FeMerge';
 import * as FeOffset from './FeOffset';
 import * as ReanimatedFeColorMatrix from './ReanimatedFeColorMatrix';
 export {
   FeColorMatrix,
+  FeFlood,
   FeGaussianBlur,
   FeMerge,
   FeOffset,
