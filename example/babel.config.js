a1be06cbfdfa6cab4fed9f708cbdfb50ab903cbb
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-10-25 14:32:57 +0200

    chore: upgrade react native in example apps (#2506)
    # Summary
    
    Update all examples to `react-native@0.76.0`

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    '@babel/plugin-proposal-export-namespace-from',
-    'module:react-native-dotenv',
-    'react-native-reanimated/plugin',
-  ],
+  plugins: ['module:react-native-dotenv', 'react-native-reanimated/plugin'],
 };
