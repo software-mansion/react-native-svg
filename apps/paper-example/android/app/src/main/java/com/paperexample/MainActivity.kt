9c7e38b065caec330e866954e6fd87276edb49cd
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-10-25 17:08:56 +0200

    chore: rename rest of paper-example

--- apps/paper-example/android/app/src/main/java/com/paperexample/MainActivity.kt
+++ apps/paper-example/android/app/src/main/java/com/paperexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.paperexample
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
