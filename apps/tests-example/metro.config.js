4449b5ce04e5e529758f3fec4e1677927669b162
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-07-31 20:15:07 +0200

    chore: Update tests-example to react-native@0.81.0-rc.3 (#2727)
    # Summary
    
    Update tests-example to `react-native@0.81.0-rc.3` to test 81 support 
    
    ## Test Plan
    
    TestsExample compiles

--- apps/tests-example/metro.config.js
+++ apps/tests-example/metro.config.js
@@ -7,7 +7,7 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const exclusionList = require('metro-config/private/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pack = require('../../package.json');
 
