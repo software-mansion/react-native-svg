62b9d78e4ca4747e984395653593dcc3781f536d
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-08-12 15:37:40 +0200

    feat: add onSvgLayout event to ios

--- src/fabric/ImageNativeComponent.ts
+++ src/fabric/ImageNativeComponent.ts
@@ -45,10 +45,12 @@ type ColorStruct = Readonly<{
 }>;
 
 type OnSvgLayoutEvent = Readonly<{
-  x: Int32;
-  y: Int32;
-  width: Int32;
-  height: Int32;
+  layout: {
+    x: Int32;
+    y: Int32;
+    width: Int32;
+    height: Int32;
+  };
 }>;
 
 interface SvgRenderableCommonProps {
