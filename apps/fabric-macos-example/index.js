c1c2a4cf2d0854b528571a662db79c08936ccbe7
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-08-18 11:07:10 +0200

    fix: remove libraries that does not support react-native-macos

--- apps/fabric-macos-example/index.js
+++ apps/fabric-macos-example/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from '../common';
+import App from '../common/noNavigationApp';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
