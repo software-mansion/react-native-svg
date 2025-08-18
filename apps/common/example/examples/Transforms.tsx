6bb303eb3375cc3061f88fd44301056c398c4256
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-08-18 13:01:29 +0200

    fix: PatternTransformExample crash on macos

--- apps/common/example/examples/Transforms.tsx
+++ apps/common/example/examples/Transforms.tsx
@@ -132,16 +132,40 @@ GradientTransformExample.title = 'Gradient transform';
 
 const icon = (
   <Svg height="30" width="30" viewBox="0 0 100 100">
-    <Pattern
-      id="p1"
-      width="0.25"
-      height="0.25"
-      patternTransform="rotate(20) skewX(30) scale(1 0.5)">
-      <Circle fill="black" cx="10" cy="10" r="10" />
-    </Pattern>
-    <Rect x="10" y="10" width="80" height="80" fill="url(#p1)" />
+    {Platform.OS === 'macos' ? (
+      <>
+        <RadialGradient
+          id="gradient1"
+          gradientUnits="userSpaceOnUse"
+          cx="100"
+          cy="100"
+          r="100"
+          fx="100"
+          fy="100">
+          <Stop offset="0%" stopColor="darkblue" />
+          <Stop offset="50%" stopColor="skyblue" />
+          <Stop offset="100%" stopColor="darkblue" />
+        </RadialGradient>
+        <Rect x="10" y="10" width="80" height="80" fill="url(#gradient1)" />
+      </>
+    ) : (
+      <>
+        <Pattern
+          id="p1"
+          width="0.25"
+          height="0.25"
+          patternTransform="rotate(20) skewX(30) scale(1 0.5)">
+          <Circle fill="black" cx="10" cy="10" r="10" />
+        </Pattern>
+        <Rect x="10" y="10" width="80" height="80" fill="url(#p1)" />
+      </>
+    )}
   </Svg>
 );
-const samples = [PatternTransformExample, GradientTransformExample];
+const samples = [GradientTransformExample];
+
+if (Platform.OS !== 'macos') {
+  samples.push(PatternTransformExample);
+}
 
 export {icon, samples};
