34561f0827023b1766b659bf2de6c425bc8c4721
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-09-22 10:01:25 +0200

    feat: add support for FeTurbulence on Android

--- android/src/main/java/com/horcrux/svg/FilterProperties.java
+++ android/src/main/java/com/horcrux/svg/FilterProperties.java
@@ -181,4 +181,68 @@ class FilterProperties {
       return type;
     }
   }
+
+  enum FeTurbulenceType {
+    FRACTAL_NOISE("fractalNoise"),
+    TURBULENCE("turbulence");
+
+    private final String type;
+
+    FeTurbulenceType(String type) {
+      this.type = type;
+    }
+
+    static FeTurbulenceType getEnum(String strVal) {
+      if (!typeToEnum.containsKey(strVal)) {
+        throw new IllegalArgumentException("Unknown String Value: " + strVal);
+      }
+      return typeToEnum.get(strVal);
+    }
+
+    private static final Map<String, FeTurbulenceType> typeToEnum = new HashMap<>();
+
+    static {
+      for (final FeTurbulenceType en : FeTurbulenceType.values()) {
+        typeToEnum.put(en.type, en);
+      }
+    }
+
+    @Nonnull
+    @Override
+    public String toString() {
+      return type;
+    }
+  }
+
+  enum FeTurbulenceStitchTile {
+    STITCH("stitch"),
+    NO_STITCH("noStitch");
+
+    private final String stitchTile;
+
+    FeTurbulenceStitchTile(String stitchTile) {
+      this.stitchTile = stitchTile;
+    }
+
+    static FeTurbulenceStitchTile getEnum(String strVal) {
+      if (!stitchTileToEnum.containsKey(strVal)) {
+        throw new IllegalArgumentException("Unknown String Value: " + strVal);
+      }
+      return stitchTileToEnum.get(strVal);
+    }
+
+    private static final Map<String, FeTurbulenceStitchTile> stitchTileToEnum = new HashMap<>();
+
+    static {
+      for (final FeTurbulenceStitchTile en : FeTurbulenceStitchTile.values()) {
+        stitchTileToEnum.put(en.stitchTile, en);
+      }
+    }
+
+    @Nonnull
+    @Override
+    public String toString() {
+      return stitchTile;
+    }
+  }
 }
