ab0f414633cb2d1cdf05bd65f57ba14f73ef45d8
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-10-07 10:26:05 +0200

    fix: starting emulator

--- apps/common/example/examples/Empty.tsx
+++ apps/common/example/examples/Empty.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
-import {Rect, Svg} from 'react-native-svg';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Defs, Path, Pattern, Rect, Svg, SvgProps} from 'react-native-svg';
 
 function EmptyExample() {
-  return <Svg height="400" width="400"></Svg>;
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: 'gray',
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: 'white',
+          }}>
+          <View style={{width: 100, height: 100}}>
+            <TestIcon color={'red'} />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 EmptyExample.title = '';
 
@@ -14,3 +48,25 @@ const icon = (
 const samples = [EmptyExample];
 
 export {icon, samples};
+
+const TestIcon = (props: SvgProps) => (
+  <Svg {...props}>
+    <Defs>
+      <Pattern
+        id={'pattern'}
+        patternUnits="userSpaceOnUse"
+        width={9}
+        height={9}>
+        <Path d="M -1,2 l 6,0" stroke="currentColor" strokeWidth={3} />
+      </Pattern>
+    </Defs>
+    <Rect x={0} y={0} width="100%" height="100%" fill={`url(#${'pattern'})`} />
+  </Svg>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
