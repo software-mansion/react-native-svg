1d6826bef324f546424fd987444c38a06de5130b
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-10-27 11:10:48 +0100

    fix: change pixel density for e2e on android

--- e2e/generateReferences.ts
+++ e2e/generateReferences.ts
@@ -2,20 +2,26 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const main = async () => {
+const generateReferences = async (
+  platform: 'android' | 'ios',
+  scaleFactor: number
+) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.setViewport({
-    height: 200,
     width: 200,
-    // This is hardcoded value which makes it possible to use only devices with pixel ratio = 3. You can change it
+    height: 200,
+    // This is value which makes it possible to use only devices with particular pixel ratio. You can change it
     // and regenerate reference images if you want to use device with different pixel ratio
     // see: https://reactnative.dev/docs/pixelratio
-    deviceScaleFactor: 3,
+    deviceScaleFactor: scaleFactor,
   });
+
   const casesPath = path.resolve('e2e', 'cases');
-  const referencesPath = path.resolve('e2e', 'references');
+  const referencesPath = path.resolve('e2e', 'references', platform);
   const cases = fs.readdirSync(casesPath);
+
   for (const testCase of cases) {
     const svgPath = path.resolve(casesPath, testCase);
     await page.goto(`file://${svgPath}`);
@@ -30,6 +36,12 @@ const main = async () => {
   }
 
   await browser.close();
+};
+
+const main = async () => {
+  await generateReferences('ios', 3.0);
+  // We use 2.625 for Android which corresponds to Pixel 7 device becuase the emulator with 3.0 is not available currently on CI
+  await generateReferences('android', 2.625);
 };
 
 main();
