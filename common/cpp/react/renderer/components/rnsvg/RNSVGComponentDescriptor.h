19e74934b73f72a6da11597767edcc6de05333da
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2025-11-12 12:50:55 +0100

    fix: stateData error

--- common/cpp/react/renderer/components/rnsvg/RNSVGComponentDescriptor.h
+++ common/cpp/react/renderer/components/rnsvg/RNSVGComponentDescriptor.h
@@ -14,11 +14,12 @@ public:
     react_native_assert(dynamic_cast<RNSVGLayoutableShadowNode *>(&shadowNode));
 
     auto layoutableShadowNode = dynamic_cast<RNSVGLayoutableShadowNode *>(&shadowNode);    
-    auto state = layoutableShadowNode->getStateData();
+    auto state = std::static_pointer_cast<const typename ShadowNodeT::ConcreteState>(shadowNode.getState());
+    auto stateData = state->getData();
 
-    if (state.getWidth() != 0 && state.getHeight() != 0) {
-      layoutableShadowNode->setSize(Size{state.getWidth(), state.getHeight()});
-      layoutableShadowNode->setShadowNodePosition(state.getX(), state.getY());
+    if (stateData.getWidth() != 0 && stateData.getHeight() != 0) {
+      layoutableShadowNode->setSize(Size{stateData.getWidth(), stateData.getHeight()});
+      layoutableShadowNode->setShadowNodePosition(stateData.getX(), stateData.getY());
     }
 
     ConcreteComponentDescriptor<ShadowNodeT>::adopt(shadowNode);
