864b6ee9f250732fb3bdd90d0b0d0605247c83d6
Kacper Żółkiewski <74975508+kacperzolkiewski@users.noreply.github.com>
2025-11-12 12:31:19 +0100

    Update common/cpp/react/renderer/components/rnsvg/RNSVGComponentDescriptor.h
    Co-authored-by: Jakub Grzywacz <jaqbgrzy@gmail.com>

--- common/cpp/react/renderer/components/rnsvg/RNSVGComponentDescriptor.h
+++ common/cpp/react/renderer/components/rnsvg/RNSVGComponentDescriptor.h
@@ -12,16 +12,15 @@ public:
   using ConcreteComponentDescriptor<ShadowNodeT>::ConcreteComponentDescriptor;
   void adopt(ShadowNode &shadowNode) const override {
     react_native_assert(dynamic_cast<RNSVGLayoutableShadowNode *>(&shadowNode));
-    auto layoutableShadowNode = dynamic_cast<RNSVGLayoutableShadowNode *>(&shadowNode);
-    
-    auto state = std::static_pointer_cast<const typename ShadowNodeT::ConcreteState>(shadowNode.getState());
-      auto stateData = state->getData();
-      
-      if (stateData.getWidth() != 0 && stateData.getHeight() != 0) {
-        layoutableShadowNode->setSize(Size{stateData.getWidth(), stateData.getHeight()});
-        layoutableShadowNode->setShadowNodePosition(stateData.getX(), stateData.getY());
-      }
-    
+
+    auto layoutableShadowNode = dynamic_cast<RNSVGLayoutableShadowNode *>(&shadowNode);    
+    auto state = layoutableShadowNode->getStateData();
+
+    if (state.getWidth() != 0 && state.getHeight() != 0) {
+      layoutableShadowNode->setSize(Size{state.getWidth(), state.getHeight()});
+      layoutableShadowNode->setShadowNodePosition(state.getX(), state.getY());
+    }
+
     ConcreteComponentDescriptor<ShadowNodeT>::adopt(shadowNode);
   }
 };
