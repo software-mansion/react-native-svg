d9c02d5b748f087ba43b87278183e0032fa56884
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-12-01 16:07:06 +0100

    fix: set absolute fill position in shadow nodes to fix onPress hit test in js (#2840)
    # Summary
    
    Sets all SVG elements position to absolute fill instead of `{0, 0, 0,
    0}` in their shadow nodes to return `Svg` element layout metrics in
    measure function to fix `onPress` handling in js.
    
    A limitation of this approach is that touch cancellation by dragging the
    finger outside the element is not supported - touch will only cancel if
    moved outside the entire SVG element.
    
    ## Test Plan
    
    Test `FabricExample`

--- common/cpp/react/renderer/components/rnsvg/RNSVGLayoutableShadowNode.cpp
+++ common/cpp/react/renderer/components/rnsvg/RNSVGLayoutableShadowNode.cpp
@@ -8,28 +8,28 @@ RNSVGLayoutableShadowNode::RNSVGLayoutableShadowNode(
     const ShadowNodeFamily::Shared &family,
     ShadowNodeTraits traits)
     : YogaLayoutableShadowNode(fragment, family, traits) {
-  if (std::strcmp(this->getComponentName(), "RNSVGGroup") != 0) {
-    setZeroDimensions();
-  }
+  updatePosition();
 }
 
 RNSVGLayoutableShadowNode::RNSVGLayoutableShadowNode(
     const ShadowNode &sourceShadowNode,
     const ShadowNodeFragment &fragment)
     : YogaLayoutableShadowNode(sourceShadowNode, fragment) {
-  if (std::strcmp(this->getComponentName(), "RNSVGGroup") != 0) {
-    setZeroDimensions();
-  }
+  updatePosition();
 }
 
-void RNSVGLayoutableShadowNode::setZeroDimensions() {
+void RNSVGLayoutableShadowNode::updatePosition() {
   // SVG handles its layout manually on the native side and does not depend on
-  // the Yoga layout. Setting the dimensions to 0 eliminates randomly positioned
-  // views in the layout inspector when Yoga attempts to interpret SVG
-  // properties like width when viewBox scale is set.
+  // the Yoga layout. Setting the dimensions to absolute fill is a hack for measure 
+  // to return "correct" layout metrics when onPress is tested. It also eliminates 
+  // randomly positioned views in the layout inspector when Yoga attempts to interpret 
+  // SVG properties like width when viewBox scale is set. Ideal solution would be to 
+  // return correct bounding box from native side in getLayoutMetrics.
   auto style = yogaNode_.style();
-  style.setDimension(yoga::Dimension::Width, yoga::StyleSizeLength::points(0));
-  style.setDimension(yoga::Dimension::Height, yoga::StyleSizeLength::points(0));
+  style.setPositionType(yoga::PositionType::Absolute);
+  style.setPosition(yoga::Edge::All, yoga::Style::Length::points(0));
+  style.setDimension(yoga::Dimension::Width, yoga::StyleSizeLength::percent(100));
+  style.setDimension(yoga::Dimension::Height, yoga::StyleSizeLength::percent(100));
   yogaNode_.setStyle(style);
 }
 
